2f20676a25d9a8a7f312c4b8d576403e1973f563
Programistich <35292229+Programistich@users.noreply.github.com>
2025-11-07 19:12:39 +0200

    tmp solution

--- entrypoint/build.gradle.kts
+++ entrypoint/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.kotlin.konan.target.Family
+
 plugins {
     id("org.jetbrains.kotlin.multiplatform")
     id("ru.astrainteractive.gradleplugin.java.core")
@@ -18,61 +21,115 @@ kotlin {
     macosArm64()
 
     applyDefaultHierarchyTemplate()
+
+    targets
+        .filterIsInstance<KotlinNativeTarget>()
+        .filter { it.konanTarget.family == Family.IOS || it.konanTarget.family == Family.OSX }
+        .forEach {
+            it.binaries.framework {
+                baseName = "BridgeConnection"
+                isStatic = true
+
+                export(projects.components.di)
+                export(projects.components.ktx)
+                export(projects.components.log)
+                export(projects.components.principal.api)
+                export(projects.components.cloud.api)
+
+                export(projects.components.bridge.config.api)
+                export(projects.components.bridge.connectionbuilder.api)
+                export(projects.components.bridge.connectionbuilder.impl)
+                export(projects.components.bridge.device.bsb.api)
+                export(projects.components.bridge.device.bsb.impl)
+                export(projects.components.bridge.device.common.api)
+                export(projects.components.bridge.feature.battery.api)
+                export(projects.components.bridge.feature.battery.impl)
+                export(projects.components.bridge.feature.common.api)
+                export(projects.components.bridge.feature.firmwareUpdate.api)
+                export(projects.components.bridge.feature.firmwareUpdate.impl)
+                export(projects.components.bridge.feature.info.api)
+                export(projects.components.bridge.feature.info.impl)
+                export(projects.components.bridge.feature.link.api)
+                export(projects.components.bridge.feature.link.impl)
+                export(projects.components.bridge.feature.provider.api)
+                export(projects.components.bridge.feature.provider.impl)
+                export(projects.components.bridge.feature.rpc.api)
+                export(projects.components.bridge.feature.rpc.impl)
+                export(projects.components.bridge.feature.screenStreaming.api)
+                export(projects.components.bridge.feature.screenStreaming.impl)
+                export(projects.components.bridge.feature.sync.impl)
+                export(projects.components.bridge.feature.wifi.api)
+                export(projects.components.bridge.feature.wifi.impl)
+                export(projects.components.bridge.orchestrator.api)
+                export(projects.components.bridge.orchestrator.impl)
+                export(projects.components.bridge.service.api)
+                export(projects.components.bridge.service.impl)
+
+                export(projects.components.bridge.transport.ble.api)
+                export(projects.components.bridge.transport.common.api)
+                export(projects.components.bridge.transport.common.impl)
+                export(projects.components.bridge.transport.mock.api)
+                export(projects.components.bridge.transport.mock.impl)
+                export(projects.components.bridge.transportconfigbuilder.api)
+                export(projects.components.bridge.transportconfigbuilder.impl)
+                export(libs.kotlin.coroutines)
+                export(projects.components.bridge.transport.ble.impl)
+            }
+        }
 }
 
 kotlin {
     sourceSets.commonMain.dependencies {
-        implementation(projects.components.di)
-        implementation(projects.components.ktx)
-        implementation(projects.components.log)
+        api(projects.components.di)
+        api(projects.components.ktx)
+        api(projects.components.log)
         api(projects.components.principal.api)
         api(projects.components.cloud.api)
 
         api(projects.components.bridge.config.api)
-        implementation(projects.components.bridge.connectionbuilder.api)
-        implementation(projects.components.bridge.connectionbuilder.impl)
-        implementation(projects.components.bridge.device.bsb.api)
-        implementation(projects.components.bridge.device.bsb.impl)
+        api(projects.components.bridge.connectionbuilder.api)
+        api(projects.components.bridge.connectionbuilder.impl)
+        api(projects.components.bridge.device.bsb.api)
+        api(projects.components.bridge.device.bsb.impl)
         api(projects.components.bridge.device.common.api)
         api(projects.components.bridge.feature.battery.api)
-        implementation(projects.components.bridge.feature.battery.impl)
-        implementation(projects.components.bridge.feature.common.api)
+        api(projects.components.bridge.feature.battery.impl)
+        api(projects.components.bridge.feature.common.api)
         api(projects.components.bridge.feature.firmwareUpdate.api)
-        implementation(projects.components.bridge.feature.firmwareUpdate.impl)
+        api(projects.components.bridge.feature.firmwareUpdate.impl)
         api(projects.components.bridge.feature.info.api)
-        implementation(projects.components.bridge.feature.info.impl)
+        api(projects.components.bridge.feature.info.impl)
         api(projects.components.bridge.feature.link.api)
-        implementation(projects.components.bridge.feature.link.impl)
+        api(projects.components.bridge.feature.link.impl)
         api(projects.components.bridge.feature.provider.api)
-        implementation(projects.components.bridge.feature.provider.impl)
-        implementation(projects.components.bridge.feature.rpc.api)
-        implementation(projects.components.bridge.feature.rpc.impl)
+        api(projects.components.bridge.feature.provider.impl)
+        api(projects.components.bridge.feature.rpc.api)
+        api(projects.components.bridge.feature.rpc.impl)
         api(projects.components.bridge.feature.screenStreaming.api)
-        implementation(projects.components.bridge.feature.screenStreaming.impl)
-        implementation(projects.components.bridge.feature.sync.impl)
+        api(projects.components.bridge.feature.screenStreaming.impl)
+        api(projects.components.bridge.feature.sync.impl)
         api(projects.components.bridge.feature.wifi.api)
-        implementation(projects.components.bridge.feature.wifi.impl)
+        api(projects.components.bridge.feature.wifi.impl)
         api(projects.components.bridge.orchestrator.api)
-        implementation(projects.components.bridge.orchestrator.impl)
+        api(projects.components.bridge.orchestrator.impl)
         api(projects.components.bridge.service.api)
-        implementation(projects.components.bridge.service.impl)
+        api(projects.components.bridge.service.impl)
 
-        implementation(projects.components.bridge.transport.ble.api)
-        implementation(projects.components.bridge.transport.common.api)
-        implementation(projects.components.bridge.transport.common.impl)
-        implementation(projects.components.bridge.transport.mock.api)
-        implementation(projects.components.bridge.transport.mock.impl)
-        implementation(projects.components.bridge.transportconfigbuilder.api)
-        implementation(projects.components.bridge.transportconfigbuilder.impl)
-        implementation(libs.kotlin.coroutines)
+        api(projects.components.bridge.transport.ble.api)
+        api(projects.components.bridge.transport.common.api)
+        api(projects.components.bridge.transport.common.impl)
+        api(projects.components.bridge.transport.mock.api)
+        api(projects.components.bridge.transport.mock.impl)
+        api(projects.components.bridge.transportconfigbuilder.api)
+        api(projects.components.bridge.transportconfigbuilder.impl)
+        api(libs.kotlin.coroutines)
     }
     sourceSets.androidMain.dependencies {
         api(projects.components.bridge.device.firstpair.connection.api)
-        implementation(projects.components.bridge.device.firstpair.connection.impl)
-        implementation(projects.components.bridge.transport.ble.impl)
+        api(projects.components.bridge.device.firstpair.connection.impl)
+        api(projects.components.bridge.transport.ble.impl)
     }
     sourceSets.appleMain.dependencies {
-        api(projects.components.bridge.device.firstpair.connection.api)
-        implementation(projects.components.bridge.transport.ble.impl)
+        api(projects.components.bridge.transport.ble.impl)
     }
 }
--- entrypoint/src/appleMain/kotlin/com/flipperdevices/busylib/BUSYLibiOS.kt
+++ entrypoint/src/appleMain/kotlin/com/flipperdevices/busylib/BUSYLibiOS.kt
@@ -1,33 +1,33 @@
-//package com.flipperdevices.busylib
-//
-//import com.flipperdevices.bridge.connection.config.api.FDevicePersistedStorage
-//import com.flipperdevices.bridge.connection.service.api.FConnectionService
-//import com.flipperdevices.bsb.auth.principal.api.BsbUserPrincipalApi
-//import com.flipperdevices.bsb.cloud.api.BSBBarsApi
-//import com.flipperdevices.busylib.di.BUSYLibGraphiOS
-//import dev.zacsweers.metro.Inject
-//import dev.zacsweers.metro.createGraphFactory
-//import kotlinx.coroutines.CoroutineScope
-//
-//@Inject
-//class BUSYLibiOS(
-//    override val connectionService: FConnectionService
-//) : BUSYLib {
-//    companion object {
-//        fun build(
-//            scope: CoroutineScope,
-//            principalApi: BsbUserPrincipalApi,
-//            bsbBarsApi: BSBBarsApi,
-//            persistedStorage: FDevicePersistedStorage,
-//        ): BUSYLibiOS {
-//            val graph = createGraphFactory<BUSYLibGraphiOS.Factory>()
-//                .create(
-//                    scope,
-//                    principalApi,
-//                    bsbBarsApi,
-//                    persistedStorage
-//                )
-//            return graph.busyLib
-//        }
-//    }
-//}
+package com.flipperdevices.busylib
+
+import com.flipperdevices.bridge.connection.config.api.FDevicePersistedStorage
+import com.flipperdevices.bridge.connection.service.api.FConnectionService
+import com.flipperdevices.bsb.auth.principal.api.BsbUserPrincipalApi
+import com.flipperdevices.bsb.cloud.api.BSBBarsApi
+import com.flipperdevices.busylib.di.BUSYLibGraphiOS
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.createGraphFactory
+import kotlinx.coroutines.CoroutineScope
+
+@Inject
+class BUSYLibiOS(
+    override val connectionService: FConnectionService
+) : BUSYLib {
+    companion object {
+        fun build(
+            scope: CoroutineScope,
+            principalApi: BsbUserPrincipalApi,
+            bsbBarsApi: BSBBarsApi,
+            persistedStorage: FDevicePersistedStorage,
+        ): BUSYLibiOS {
+            val graph = createGraphFactory<BUSYLibGraphiOS.Factory>()
+                .create(
+                    scope,
+                    principalApi,
+                    bsbBarsApi,
+                    persistedStorage
+                )
+            return graph.busyLib
+        }
+    }
+}
