e7dffe3b3467e35f599cbfba25103f309d13b3d5
Roman Makeev <makeevrserg@gmail.com>
2025-11-07 03:52:10 +0300

    fix desktop

--- instances/multiplatform/src/jvmMain/kotlin/com/flipperdevices/bridge/connection/App.kt
+++ instances/multiplatform/src/jvmMain/kotlin/com/flipperdevices/bridge/connection/App.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.lightColors
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -13,8 +15,6 @@ import com.arkivanov.decompose.extensions.compose.lifecycle.LifecycleController
 import com.arkivanov.essenty.lifecycle.LifecycleRegistry
 import com.flipperdevices.bridge.connection.di.AppComponent
 import com.flipperdevices.bridge.connection.utils.runOnUiThread
-import com.flipperdevices.bsb.core.theme.BusyBarTheme
-import com.flipperdevices.bsb.core.theme.LocalV2Pallet
 
 fun launch(appComponent: AppComponent) {
     val lifecycle = LifecycleRegistry()
@@ -32,14 +32,16 @@ fun launch(appComponent: AppComponent) {
             state = windowState,
             title = "BUSY Bar App",
         ) {
-            BusyBarTheme(darkMode = true) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(LocalV2Pallet.current.background.base)
-                        .safeDrawingPadding()
-                ) {
-                    root.Render(Modifier)
+            MaterialTheme {
+                MaterialTheme(lightColors()) {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(MaterialTheme.colors.background)
+                            .safeDrawingPadding()
+                    ) {
+                        root.Render(Modifier)
+                    }
                 }
             }
         }
