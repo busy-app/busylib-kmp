63f2a4f0f413e2a91fcf992d628fd7a74a7fae5f
Nikita Kulikov <n@kulikov.uk>
2025-11-07 17:29:49 +0000

    Implement busylib ios

---
+++ entrypoint/src/appleMain/kotlin/com/flipperdevices/busylib/BUSYLibiOS.kt
@@ -0,0 +1,37 @@
+package com.flipperdevices.busylib
+
+import com.flipperdevices.bridge.connection.config.api.FDevicePersistedStorage
+import com.flipperdevices.bridge.connection.feature.provider.api.FFeatureProvider
+import com.flipperdevices.bridge.connection.orchestrator.api.FDeviceOrchestrator
+import com.flipperdevices.bridge.connection.service.api.FConnectionService
+import com.flipperdevices.bsb.auth.principal.api.BsbUserPrincipalApi
+import com.flipperdevices.bsb.cloud.api.BSBBarsApi
+import com.flipperdevices.busylib.di.BUSYLibGraphIOS
+import dev.zacsweers.metro.Inject
+import dev.zacsweers.metro.createGraphFactory
+import kotlinx.coroutines.CoroutineScope
+
+@Inject
+class BUSYLibIOS(
+    override val connectionService: FConnectionService,
+    override val orchestrator: FDeviceOrchestrator,
+    override val featureProvider: FFeatureProvider
+) : BUSYLib {
+    companion object {
+        fun build(
+            scope: CoroutineScope,
+            principalApi: BsbUserPrincipalApi,
+            bsbBarsApi: BSBBarsApi,
+            persistedStorage: FDevicePersistedStorage,
+        ): BUSYLibIOS {
+            val graph = createGraphFactory<BUSYLibGraphIOS.Factory>()
+                .create(
+                    scope,
+                    principalApi,
+                    bsbBarsApi,
+                    persistedStorage
+                )
+            return graph.busyLib
+        }
+    }
+}
