99a47984fcf1ce726137a2ba3524aa982c5ca7fe
Roman Makeev <makeevrserg@gmail.com>
2025-11-08 06:37:52 +0300

    fix detelt and publish

--- entrypoint/src/commonMain/kotlin/com/flipperdevices/busylib/di/RootModule.kt
+++ entrypoint/src/commonMain/kotlin/com/flipperdevices/busylib/di/RootModule.kt
@@ -3,14 +3,11 @@ package com.flipperdevices.busylib.di
 import com.flipperdevices.bridge.connection.config.api.FDevicePersistedStorage
 import com.flipperdevices.bsb.auth.principal.api.BsbUserPrincipalApi
 import com.flipperdevices.bsb.cloud.api.BSBBarsApi
-import kotlinx.coroutines.CoroutineScope
 
 @Suppress("LongParameterList")
 class RootModule(
-    val scope: CoroutineScope,
     val bsbBarsApi: BSBBarsApi,
     val scannerModule: ScannerModule,
-    val platformModule: PlatformModule,
     val fDeviceHolderFactoryModule: FDeviceHolderFactoryModule,
     val principalApi: BsbUserPrincipalApi,
     val persistedStorage: FDevicePersistedStorage,
