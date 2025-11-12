bfaaed2e3e1e684af88b9f33d7385b1dbe1bf1f5
Programistich <35292229+Programistich@users.noreply.github.com>
2025-11-12 16:17:42 +0200

    flow

--- sample/src/commonMain/kotlin/net/flipper/bridge/connection/utils/principal/impl/UserPrincipalApiNoop.kt
+++ sample/src/commonMain/kotlin/net/flipper/bridge/connection/utils/principal/impl/UserPrincipalApiNoop.kt
@@ -1,7 +1,6 @@
 package net.flipper.bridge.connection.utils.principal.impl
 
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asSharedFlow
 import net.flipper.bsb.auth.principal.api.BsbUserPrincipal
 import net.flipper.bsb.auth.principal.api.BsbUserPrincipalApi
 import net.flipper.core.busylib.ktx.common.wrap
