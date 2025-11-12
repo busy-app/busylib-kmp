bbafd408c3c1a31ffd1632e3af33baa9ae1cf7a8
Programistich <35292229+Programistich@users.noreply.github.com>
2025-11-12 15:48:41 +0200

    Flow for ios

--- components/ktx/src/commonMain/kotlin/net/flipper/core/busylib/ktx/common/FlowKtx.kt
+++ components/ktx/src/commonMain/kotlin/net/flipper/core/busylib/ktx/common/FlowKtx.kt
@@ -1,6 +1,15 @@
 package net.flipper.core.busylib.ktx.common
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalForInheritanceCoroutinesApi
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 fun <T> Flow<T>?.orEmpty(): Flow<T> = this ?: emptyFlow()
