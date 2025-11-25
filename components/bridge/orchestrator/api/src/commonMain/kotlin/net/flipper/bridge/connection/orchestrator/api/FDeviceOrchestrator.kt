fd4656e14670c4dc7e993bba57b05285aa2f40b8
Roman Makeev <makeevrserg@gmail.com>
2025-11-25 17:01:58 +0300

    remove reconnect

--- components/bridge/orchestrator/api/src/commonMain/kotlin/net/flipper/bridge/connection/orchestrator/api/FDeviceOrchestrator.kt
+++ components/bridge/orchestrator/api/src/commonMain/kotlin/net/flipper/bridge/connection/orchestrator/api/FDeviceOrchestrator.kt
@@ -1,6 +1,5 @@
 package net.flipper.bridge.connection.orchestrator.api
 
-import kotlinx.coroutines.flow.first
 import net.flipper.bridge.connection.config.api.model.FDeviceBaseModel
 import net.flipper.bridge.connection.orchestrator.api.model.FDeviceConnectStatus
 import net.flipper.busylib.core.wrapper.WrappedStateFlow
@@ -12,19 +11,4 @@ interface FDeviceOrchestrator {
     suspend fun connect(config: FDeviceBaseModel)
 
     suspend fun disconnectCurrent()
-}
-
-suspend fun FDeviceOrchestrator.reconnect(): Result<Unit> {
-    val latestDevice = when (val state = getState().first()) {
-        is FDeviceConnectStatus.Connected -> state.device
-        is FDeviceConnectStatus.Connecting -> state.device
-        is FDeviceConnectStatus.Disconnected -> state.device
-        is FDeviceConnectStatus.Disconnecting -> state.device
-    }
-    if (latestDevice == null) {
-        return Result.failure(IllegalStateException("Latest device was null"))
-    }
-    disconnectCurrent()
-    connect(latestDevice)
-    return Result.success(Unit)
 }
