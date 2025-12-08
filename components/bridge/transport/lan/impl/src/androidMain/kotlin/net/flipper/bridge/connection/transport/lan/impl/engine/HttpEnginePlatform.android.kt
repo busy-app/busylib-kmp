7917f6295be86a8c799b9d2d928f1e2486323fb5
Nikita Kulikov <n@kulikov.uk>
2025-12-08 12:03:52 +0000

    Fix android platform engine

--- components/bridge/transport/lan/impl/src/androidMain/kotlin/net/flipper/bridge/connection/transport/lan/impl/engine/HttpEnginePlatform.android.kt
+++ components/bridge/transport/lan/impl/src/androidMain/kotlin/net/flipper/bridge/connection/transport/lan/impl/engine/HttpEnginePlatform.android.kt
@@ -1,5 +1,21 @@
 package net.flipper.bridge.connection.transport.lan.impl.engine
 
-actual fun getPlatformEngineFactory(): io.ktor.client.engine.HttpClientEngineFactory<*> {
-    TODO("Not yet implemented")
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.HttpClientEngineFactory
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.engine.okhttp.OkHttpConfig
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
+
+actual fun getPlatformEngineFactory(): HttpClientEngineFactory<*> {
+    return object : HttpClientEngineFactory<OkHttpConfig> {
+        override fun create(block: OkHttpConfig.() -> Unit): HttpClientEngine {
+            return OkHttp.create {
+                block()
+                preconfigured = OkHttpClient.Builder()
+                    .pingInterval(PING_INTERVAL.inWholeMilliseconds, TimeUnit.MILLISECONDS)
+                    .build()
+            }
+        }
+    }
 }
