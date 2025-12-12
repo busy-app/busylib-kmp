8d22cd16c60509d19a3e16a53c871e1f612720e9
Programistich <35292229+Programistich@users.noreply.github.com>
2025-12-12 17:09:22 +0200

    move checker

--- sample/src/macosMain/kotlin/net/flipper/bridge/connection/utils/PermissionCheckerNoop.kt
+++
@@ -1,7 +0,0 @@
-package net.flipper.bridge.connection.utils
-
-import net.flipper.bridge.connection.screens.utils.PermissionChecker
-
-class PermissionCheckerNoop : PermissionChecker {
-    override fun isPermissionGranted() = true
-}
