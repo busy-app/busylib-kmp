70d20b693f41ca063082f343fef45e0562d9babb
Roman Makeev <makeevrserg@gmail.com>
2026-01-10 19:44:01 +0300

    fix redundant mutex

--- components/core/ktx/src/commonMain/kotlin/net/flipper/core/busylib/ktx/common/cache/DefaultObjectCache.kt
+++ components/core/ktx/src/commonMain/kotlin/net/flipper/core/busylib/ktx/common/cache/DefaultObjectCache.kt
@@ -17,19 +17,17 @@ import kotlin.time.TimeSource
 
 class DefaultObjectCache(
     private val aliveAfterRead: Duration = 5.seconds,
-    private val aliveAfterWrite: Duration = Duration.Companion.INFINITE
+    private val aliveAfterWrite: Duration = Duration.INFINITE
 ) : ObjectCache, LogTagProvider by TaggedLogger("DefaultObjectCache") {
     private val mutex = Mutex()
     private val cache = mutableMapOf<KClass<*>, CacheEntry<*>>()
 
     sealed interface CacheEntry<T : Any> {
-        val mutex: Mutex
 
-        data class Pending(override val mutex: Mutex) : CacheEntry<Nothing>
+        data object Pending : CacheEntry<Nothing>
 
         data class Created<T : Any>(
             val deferredValue: Deferred<T>,
-            override val mutex: Mutex
         ) : CacheEntry<T> {
             val writtenAt: TimeSource.Monotonic.ValueTimeMark = TimeSource.Monotonic.markNow()
             var lastReadAt: TimeSource.Monotonic.ValueTimeMark = TimeSource.Monotonic.markNow()
@@ -46,27 +44,21 @@ class DefaultObjectCache(
     /**
      * Clear expired entries only if entry's mutex is not locked
      */
-    private fun clearExpired() {
+    private fun clearExpiredUnsafe() {
         cache
             .toMap()
             .filter { (_, value) -> (value as? CacheEntry.Created<*>?)?.isExpired == true }
-            .filter { (_, value) -> value.mutex.tryLock() }
-            .forEach { (clazz, value) ->
-                cache.remove(clazz)
-                value.mutex.unlock()
-            }
+            .forEach { (clazz, _) -> cache.remove(clazz) }
     }
 
     private suspend fun <T : Any> CoroutineScope.replaceEntry(
         clazz: KClass<T>,
-        entityMutex: Mutex,
         block: suspend () -> T
     ): CacheEntry.Created<T> {
         val newEntry = CacheEntry.Created(
             deferredValue = async { block.invoke() },
-            mutex = entityMutex
         )
-        mutex.withLock { cache.put(clazz, newEntry) }
+        cache[clazz] = newEntry
         return newEntry
     }
 
@@ -77,30 +69,20 @@ class DefaultObjectCache(
     ): Deferred<T> = coroutineScope {
         withContext(NonCancellable) {
             mutex.withLock {
-                clearExpired()
-                val entry = cache.getOrPut(clazz) { CacheEntry.Pending(Mutex()) }
-                async {
-                    entry.mutex.withLock {
-                        (entry as? CacheEntry.Created<*>)
-                            ?.let { entry -> entry.deferredValue as? Deferred<T> }
-                            ?.takeIf { !ignoreCache }
-                            ?: this@coroutineScope.replaceEntry(
-                                clazz = clazz,
-                                entityMutex = entry.mutex,
-                                block = block
-                            ).deferredValue
-                    }
-                }
-            }.await()
+                clearExpiredUnsafe()
+                val entry = cache.getOrPut(clazz) { CacheEntry.Pending }
+                (entry as? CacheEntry.Created<*>)
+                    ?.let { entry -> entry.deferredValue as? Deferred<T> }
+                    ?.takeIf { !ignoreCache }
+                    ?: this@coroutineScope.replaceEntry(
+                        clazz = clazz,
+                        block = block
+                    ).deferredValue
+            }
         }
     }
 
     override suspend fun clear() {
-        mutex.withLock {
-            val mutexes = cache.map { (_, entry) -> entry.mutex }
-            mutexes.forEach { mutex -> mutex.lock() }
-            cache.clear()
-            mutexes.forEach { mutex -> mutex.unlock() }
-        }
+        mutex.withLock { cache.clear() }
     }
 }
