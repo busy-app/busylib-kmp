68735a00e6f1164b5c3d087b09082881acab32dd
Nikita Kulikov <n@kulikov.uk>
2026-01-12 17:45:31 +0000

    Add POC for connection service logic

--- components/bridge/service/impl/src/commonMain/kotlin/net/flipper/bridge/connection/service/impl/FConnectionServiceImpl.kt
+++ components/bridge/service/impl/src/commonMain/kotlin/net/flipper/bridge/connection/service/impl/FConnectionServiceImpl.kt
@@ -3,8 +3,12 @@ package net.flipper.bridge.connection.service.impl
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.cache
 import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
@@ -13,6 +17,7 @@ import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import me.tatarka.inject.annotations.Inject
 import net.flipper.bridge.connection.config.api.FDevicePersistedStorage
+import net.flipper.bridge.connection.config.api.model.FDeviceBaseModel
 import net.flipper.bridge.connection.orchestrator.api.FDeviceOrchestrator
 import net.flipper.bridge.connection.orchestrator.api.model.DisconnectStatus
 import net.flipper.bridge.connection.orchestrator.api.model.FDeviceConnectStatus
@@ -23,6 +28,14 @@ import net.flipper.core.busylib.log.LogTagProvider
 import net.flipper.core.busylib.log.warn
 import software.amazon.lastmile.kotlin.inject.anvil.ContributesBinding
 import software.amazon.lastmile.kotlin.inject.anvil.SingleIn
+
+
+sealed interface ExpectedState {
+    data object Disconnected : ExpectedState
+
+    data class Connected(val device: FDeviceBaseModel) : ExpectedState
+}
+
 
 @SingleIn(BusyLibGraph::class)
 @Inject
@@ -35,37 +48,54 @@ class FConnectionServiceImpl(
 
     private val scope = CoroutineScope(SupervisorJob() + FlipperDispatchers.default)
     private val mutex = Mutex()
-    private val isForceDisconnected = MutableStateFlow(false)
+    private val isForceDisconnectedFlow = MutableStateFlow(false)
 
-    private fun getBrokenConnectionReconnectJob(scope: CoroutineScope): Job {
-        return orchestrator.getState()
-            .onEach { status ->
-                if (status !is FDeviceConnectStatus.Disconnected) return@onEach
-                when (status.reason) {
-                    DisconnectStatus.NOT_INITIALIZED -> return@onEach
-                    DisconnectStatus.REPORTED_BY_TRANSPORT -> Unit
-                    DisconnectStatus.ERROR_UNKNOWN -> Unit
-                }
-                if (isForceDisconnected.first()) return@onEach
-                val currentDevice = status.device ?: return@onEach
-                orchestrator.disconnectCurrent()
-                orchestrator.connect(currentDevice)
-            }.launchIn(scope)
+    private fun getExpectedState(): Flow<ExpectedState> {
+        return combine(
+            isForceDisconnectedFlow,
+            fDevicePersistedStorage.getCurrentDevice()
+        ) { isForceDisconnected, currentDevice ->
+            if (isForceDisconnected) {
+                return@combine ExpectedState.Disconnected
+            }
+            if (currentDevice == null) {
+                return@combine ExpectedState.Disconnected
+            }
+            return@combine ExpectedState.Connected(currentDevice)
+        }.distinctUntilChanged()
     }
+
 
     private fun getConnectionJob(scope: CoroutineScope): Job {
         return combine(
-            flow = fDevicePersistedStorage.getCurrentDevice(),
-            flow2 = isForceDisconnected,
-            transform = { currentDevice, isForceDisconnected ->
-                when {
-                    isForceDisconnected -> orchestrator.disconnectCurrent()
-                    currentDevice == null -> orchestrator.disconnectCurrent()
+            flow = getExpectedState(),
+            flow2 = orchestrator.getState()
+        ) { expectedState, realState ->
+            when (realState) {
+                is FDeviceConnectStatus.Connected -> when (expectedState) {
+                    is ExpectedState.Connected -> if (expectedState.device != realState.device) {
+                        orchestrator.connect(expectedState.device)
+                    }
 
-                    else -> orchestrator.connect(currentDevice)
+                    ExpectedState.Disconnected -> orchestrator.disconnectCurrent()
                 }
+
+                is FDeviceConnectStatus.Disconnected -> when (expectedState) {
+                    is ExpectedState.Connected -> orchestrator.connect(expectedState.device)
+                    ExpectedState.Disconnected -> Unit
+                }
+
+                is FDeviceConnectStatus.Connecting -> when (expectedState) {
+                    is ExpectedState.Connected -> if (expectedState.device != realState.device) {
+                        orchestrator.connect(expectedState.device)
+                    }
+
+                    ExpectedState.Disconnected -> orchestrator.disconnectCurrent()
+                }
+
+                is FDeviceConnectStatus.Disconnecting -> Unit // Transition state
             }
-        ).launchIn(scope)
+        }.launchIn(scope)
     }
 
     override fun onApplicationInit() {
@@ -75,29 +105,27 @@ class FConnectionServiceImpl(
                 return@launch
             }
             mutex.withLock {
-                val brokenConnectionReconnectJob = getBrokenConnectionReconnectJob(this)
                 val connectionJob = getConnectionJob(this)
                 connectionJob.join()
-                brokenConnectionReconnectJob.join()
             }
         }
     }
 
     override fun connectCurrent() {
         scope.launch {
-            isForceDisconnected.emit(false)
+            isForceDisconnectedFlow.emit(false)
         }
     }
 
     override fun disconnect() {
         scope.launch {
-            isForceDisconnected.emit(true)
+            isForceDisconnectedFlow.emit(true)
         }
     }
 
     override fun forgetCurrentDevice() {
         scope.launch {
-            isForceDisconnected.emit(true)
+            isForceDisconnectedFlow.emit(true)
             fDevicePersistedStorage.getCurrentDevice()
                 .first()
                 ?.let { currentDevice ->
