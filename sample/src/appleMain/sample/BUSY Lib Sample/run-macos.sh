0441333ae5d50004672e0ee9a8464e5a36d70398
Nikita Kulikov <n@kulikov.uk>
2026-01-21 16:36:34 +0000

    Fix apple samples

--- sample/src/appleMain/sample/BUSY Lib Sample/run-macos.sh
+++
@@ -1,8 +0,0 @@
-xcodebuild \
-  -scheme "BUSY Lib Sample" \
-  -destination "platform=macOS,arch=arm64" \
-  -configuration Debug \
-  -derivedDataPath ./build-macos \
-  build
-
-open ./build-macos/Build/Products/Debug/BUSY\ Lib\ Sample.app
