59550e3b2f6a16fbf173f23e74b42b2dd7c4afbb
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2026-01-21 18:09:40 +0200

    Use result wrapper (#69)

--- sample/src/commonMain/kotlin/net/flipper/bridge/connection/utils/cloud/BUSYLibBarsApiNoop.kt
+++ sample/src/commonMain/kotlin/net/flipper/bridge/connection/utils/cloud/BUSYLibBarsApiNoop.kt
@@ -2,12 +2,13 @@ package net.flipper.bridge.connection.utils.cloud
 
 import net.flipper.bsb.auth.principal.api.BUSYLibUserPrincipal
 import net.flipper.bsb.cloud.api.BUSYLibBarsApi
+import net.flipper.busylib.core.wrapper.CResult
 
 class BUSYLibBarsApiNoop : BUSYLibBarsApi {
     override suspend fun registerBusyBar(
         principal: BUSYLibUserPrincipal.Token,
         pin: String
-    ): Result<Unit> {
-        return Result.success(Unit)
+    ): CResult<Unit> {
+        return CResult.success(Unit)
     }
 }
