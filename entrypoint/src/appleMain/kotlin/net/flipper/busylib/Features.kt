70b0e5c6c5c54fc8e6199ec04e375a74ffc03f73
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2026-01-22 17:29:51 +0200

    All features for apple (#73)

--- entrypoint/src/appleMain/kotlin/net/flipper/busylib/Features.kt
+++ entrypoint/src/appleMain/kotlin/net/flipper/busylib/Features.kt
@@ -1,18 +1,31 @@
+@file:Suppress("TooManyFunctions")
+
 package net.flipper.busylib
 
 import net.flipper.bridge.connection.feature.battery.api.FDeviceBatteryInfoFeatureApi
+import net.flipper.bridge.connection.feature.ble.api.FBleFeatureApi
+import net.flipper.bridge.connection.feature.firmwareupdate.api.FFirmwareUpdateFeatureApi
+import net.flipper.bridge.connection.feature.info.api.FDeviceInfoFeatureApi
 import net.flipper.bridge.connection.feature.link.check.ondemand.api.FLinkedInfoOnDemandFeatureApi
 import net.flipper.bridge.connection.feature.oncall.api.FOnCallFeatureApi
 import net.flipper.bridge.connection.feature.provider.api.FFeatureProvider
 import net.flipper.bridge.connection.feature.provider.api.FFeatureStatus
+import net.flipper.bridge.connection.feature.screenstreaming.api.FScreenStreamingFeatureApi
+import net.flipper.bridge.connection.feature.settings.api.FSettingsFeatureApi
 import net.flipper.bridge.connection.feature.wifi.api.FWiFiFeatureApi
 import net.flipper.busylib.core.wrapper.WrappedFlow
 import net.flipper.busylib.core.wrapper.wrap
 
+// Battery Info Feature
 fun FFeatureProvider.getBatteryInfoFeature(): WrappedFlow<FFeatureStatus<FDeviceBatteryInfoFeatureApi>> {
     return get(FDeviceBatteryInfoFeatureApi::class).wrap()
 }
 
+suspend fun FFeatureProvider.getBatteryInfoFeatureSync(): FDeviceBatteryInfoFeatureApi? {
+    return this.getSync(FDeviceBatteryInfoFeatureApi::class)
+}
+
+// WiFi Feature
 fun FFeatureProvider.getWiFiFeature(): WrappedFlow<FFeatureStatus<FWiFiFeatureApi>> {
     return get(FWiFiFeatureApi::class).wrap()
 }
@@ -21,6 +34,7 @@ suspend fun FFeatureProvider.getWiFiFeatureSync(): FWiFiFeatureApi? {
     return this.getSync(FWiFiFeatureApi::class)
 }
 
+// Linked Info On Demand Feature
 fun FFeatureProvider.getFLinkedInfoOnDemandFeature(): WrappedFlow<FFeatureStatus<FLinkedInfoOnDemandFeatureApi>> {
     return get(FLinkedInfoOnDemandFeatureApi::class).wrap()
 }
@@ -29,6 +43,56 @@ suspend fun FFeatureProvider.getFLinkedInfoOnDemandFeatureSync(): FLinkedInfoOnD
     return this.getSync(FLinkedInfoOnDemandFeatureApi::class)
 }
 
+// OnCall Feature
+fun FFeatureProvider.getOnCallFeature(): WrappedFlow<FFeatureStatus<FOnCallFeatureApi>> {
+    return get(FOnCallFeatureApi::class).wrap()
+}
+
 suspend fun FFeatureProvider.getOnCallFeatureSync(): FOnCallFeatureApi? {
     return this.getSync(FOnCallFeatureApi::class)
+}
+
+// Settings Feature
+fun FFeatureProvider.getSettingsFeature(): WrappedFlow<FFeatureStatus<FSettingsFeatureApi>> {
+    return this.get(FSettingsFeatureApi::class).wrap()
+}
+
+suspend fun FFeatureProvider.getSettingsFeatureSync(): FSettingsFeatureApi? {
+    return this.getSync(FSettingsFeatureApi::class)
+}
+
+// BLE Feature
+fun FFeatureProvider.getBleFeature(): WrappedFlow<FFeatureStatus<FBleFeatureApi>> {
+    return get(FBleFeatureApi::class).wrap()
+}
+
+suspend fun FFeatureProvider.getBleFeatureSync(): FBleFeatureApi? {
+    return this.getSync(FBleFeatureApi::class)
+}
+
+// Device Info Feature
+fun FFeatureProvider.getDeviceInfoFeature(): WrappedFlow<FFeatureStatus<FDeviceInfoFeatureApi>> {
+    return get(FDeviceInfoFeatureApi::class).wrap()
+}
+
+suspend fun FFeatureProvider.getDeviceInfoFeatureSync(): FDeviceInfoFeatureApi? {
+    return this.getSync(FDeviceInfoFeatureApi::class)
+}
+
+// Firmware Update Feature
+fun FFeatureProvider.getFirmwareUpdateFeature(): WrappedFlow<FFeatureStatus<FFirmwareUpdateFeatureApi>> {
+    return get(FFirmwareUpdateFeatureApi::class).wrap()
+}
+
+suspend fun FFeatureProvider.getFirmwareUpdateFeatureSync(): FFirmwareUpdateFeatureApi? {
+    return this.getSync(FFirmwareUpdateFeatureApi::class)
+}
+
+// Screen Streaming Feature
+fun FFeatureProvider.getScreenStreamingFeature(): WrappedFlow<FFeatureStatus<FScreenStreamingFeatureApi>> {
+    return get(FScreenStreamingFeatureApi::class).wrap()
+}
+
+suspend fun FFeatureProvider.getScreenStreamingFeatureSync(): FScreenStreamingFeatureApi? {
+    return this.getSync(FScreenStreamingFeatureApi::class)
 }
