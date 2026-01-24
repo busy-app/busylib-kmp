dd0ff2f10a98d9f22c8c4f58b078ea88beb46b31
Roman Makeev <makeevrserg@gmail.com>
2026-01-24 12:46:02 +0300

    update agp version

--- components/core/di/build.gradle.kts
+++ components/core/di/build.gradle.kts
@@ -1,7 +1,4 @@
 plugins {
     id("flipper.multiplatform")
     id("flipper.anvil-multiplatform")
-    id("ru.astrainteractive.gradleplugin.java.core")
-    id("ru.astrainteractive.gradleplugin.android.namespace")
-    id("ru.astrainteractive.gradleplugin.android.core")
 }
