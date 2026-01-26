2d4ec84140aea36f4b2cca0f7b60a0ca01303bce
Nikita Kulikov <n@kulikov.uk>
2026-01-26 20:42:11 +0000

    Add example transport capability

--- components/bridge/transport/common/api/src/commonMain/kotlin/net/flipper/bridge/connection/transport/common/api/serial/FHTTPDeviceApi.kt
+++ components/bridge/transport/common/api/src/commonMain/kotlin/net/flipper/bridge/connection/transport/common/api/serial/FHTTPDeviceApi.kt
@@ -1,9 +1,13 @@
 package net.flipper.bridge.connection.transport.common.api.serial
 
 import io.ktor.client.engine.HttpClientEngine
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 interface FHTTPDeviceApi {
     fun getDeviceHttpEngine(): HttpClientEngine
 
-    fun hasCapability(capability: FHTTPTransportCapability): Boolean = false
+    fun hasCapability(
+        capability: FHTTPTransportCapability
+    ): StateFlow<Boolean> = MutableStateFlow(false)
 }
--- components/bridge/transport/common/api/src/commonMain/kotlin/net/flipper/bridge/connection/transport/common/api/serial/FHTTPTransportCapability.kt
+++ components/bridge/transport/common/api/src/commonMain/kotlin/net/flipper/bridge/connection/transport/common/api/serial/FHTTPTransportCapability.kt
@@ -4,4 +4,6 @@ package net.flipper.bridge.connection.transport.common.api.serial
  * Some requests can be executed only by specific transport. For example:
  * - WiFi connect can be executed only by BLE
  */
-enum class FHTTPTransportCapability
+enum class FHTTPTransportCapability {
+    BB_WEBSOCKET_SUPPORTED
+}
