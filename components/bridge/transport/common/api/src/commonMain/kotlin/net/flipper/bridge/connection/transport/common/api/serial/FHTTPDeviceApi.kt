329df6eb18032e801f69a7d02eacc8bcdd228eed
Nikita Kulikov <n@kulikov.uk>
2026-01-27 12:52:54 +0000

    Fix StateFlow

--- components/bridge/transport/common/api/src/commonMain/kotlin/net/flipper/bridge/connection/transport/common/api/serial/FHTTPDeviceApi.kt
+++ components/bridge/transport/common/api/src/commonMain/kotlin/net/flipper/bridge/connection/transport/common/api/serial/FHTTPDeviceApi.kt
@@ -3,12 +3,13 @@ package net.flipper.bridge.connection.transport.common.api.serial
 import io.ktor.client.engine.HttpClientEngine
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
 
 interface FHTTPDeviceApi {
     fun getDeviceHttpEngine(): HttpClientEngine
 
-    fun getCapabilities(): MutableStateFlow<List<FHTTPTransportCapability>> =
+    fun getCapabilities(): StateFlow<List<FHTTPTransportCapability>> =
         MutableStateFlow(emptyList())
 }
 
