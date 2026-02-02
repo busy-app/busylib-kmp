c91f3bb969b2460dd84c469d38580bc30490daa9
Nikita Kulikov <n@kulikov.uk>
2026-02-02 18:58:27 +0000

    Fix android ktor

--- components/core/ktor/src/androidMain/kotlin/net/flipper/core/ktor/HttpEnginePlatform.android.kt
+++ components/core/ktor/src/androidMain/kotlin/net/flipper/core/ktor/HttpEnginePlatform.android.kt
@@ -4,8 +4,8 @@ import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.engine.okhttp.OkHttpConfig
+import okhttp3.OkHttpClient
 import java.util.concurrent.TimeUnit
-
 actual fun getPlatformEngineFactory(): HttpClientEngineFactory<*> {
     return object : HttpClientEngineFactory<OkHttpConfig> {
         override fun create(block: OkHttpConfig.() -> Unit): HttpClientEngine {
