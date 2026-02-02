e0d8155d3fed42002b9beeec804084f68340df9a
Nikita Kulikov <n@kulikov.uk>
2026-02-02 19:04:40 +0000

    Fix doc

--- components/core/network/src/androidMain/kotlin/com/flipperdevices/core/network/BUSYLibAndroidNetworkStateApi.kt
+++ components/core/network/src/androidMain/kotlin/com/flipperdevices/core/network/BUSYLibAndroidNetworkStateApi.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.Lifecycle
 interface BUSYLibAndroidNetworkStateApi : BUSYLibNetworkStateApi {
     /**
      * Registers a lifecycle to be tracked for determining network availability.
-     * Network will only be considered available when all registered lifecycles
+     * Network will only be considered available when any of the registered lifecycles
      * are at least in STARTED state. Call this for foreground services to ensure
      * network requests respect Android 15's process lifecycle requirements.
      *
