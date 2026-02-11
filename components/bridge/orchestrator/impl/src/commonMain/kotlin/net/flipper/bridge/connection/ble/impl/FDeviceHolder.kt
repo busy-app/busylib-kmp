4517063475b6f9a0e11155eced1a047c47b09bfd
Nikita Kulikov <n@kulikov.uk>
2026-02-11 16:46:20 +0000

    Pass connection config to device holder

--- components/bridge/orchestrator/impl/src/commonMain/kotlin/net/flipper/bridge/connection/ble/impl/FDeviceHolder.kt
+++ components/bridge/orchestrator/impl/src/commonMain/kotlin/net/flipper/bridge/connection/ble/impl/FDeviceHolder.kt
@@ -2,11 +2,15 @@ package net.flipper.bridge.connection.ble.impl
 
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.async
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.cancelAndJoin
 import kotlinx.coroutines.job
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import me.tatarka.inject.annotations.Inject
 import net.flipper.bridge.connection.connectionbuilder.api.FDeviceConfigToConnection
 import net.flipper.bridge.connection.transport.common.api.FConnectedDeviceApi
@@ -14,6 +18,7 @@ import net.flipper.bridge.connection.transport.common.api.FDeviceConnectionConfi
 import net.flipper.bridge.connection.transport.common.api.FInternalTransportConnectionStatus
 import net.flipper.bridge.connection.transport.common.api.FTransportConnectionStatusListener
 import net.flipper.core.busylib.ktx.common.FlipperDispatchers
+import net.flipper.core.busylib.ktx.common.transform
 import net.flipper.core.busylib.log.LogTagProvider
 import net.flipper.core.busylib.log.info
 
@@ -25,12 +30,14 @@ class FDeviceHolderFactory(
     private val deviceConnectionHelper: FDeviceConfigToConnection
 ) {
     fun <API : FConnectedDeviceApi> build(
+        uniqueId: String,
         config: FDeviceConnectionConfig<API>,
         listener: DeviceHolderListener<API, FInternalTransportConnectionStatus>,
         onConnectError: DeviceHolderListener<API, Throwable>,
         exceptionHandler: DeviceHolderListener<API, Throwable>
     ): FDeviceHolder<API> {
         return FDeviceHolder(
+            uniqueId = uniqueId,
             config = config,
             listener = listener,
             onConnectError = onConnectError,
@@ -41,7 +48,8 @@ class FDeviceHolderFactory(
 }
 
 class FDeviceHolder<API : FConnectedDeviceApi>(
-    val config: FDeviceConnectionConfig<API>,
+    val uniqueId: String,
+    private val config: FDeviceConnectionConfig<API>,
     private val listener: DeviceHolderListener<API, FInternalTransportConnectionStatus>,
     private val onConnectError: DeviceHolderListener<API, Throwable>,
     private val deviceConnectionHelper: FDeviceConfigToConnection,
@@ -58,22 +66,30 @@ class FDeviceHolder<API : FConnectedDeviceApi>(
             exceptionHandler(this, throwable)
         }
     )
-    private var deviceApi: API? = null
-    private val connectJob: Job = scope.launch {
-        deviceApi = deviceConnectionHelper.connect(
+    private var deviceApi: Deferred<API> = scope.async {
+        deviceConnectionHelper.connect(
             scope, config, transportConnectionListener
         ).onFailure {
             onConnectError(this@FDeviceHolder, it)
-        }.getOrNull()
+        }.getOrThrow()
+    }
+
+    suspend fun tryToUpdateConnectionConfig(
+        config: FDeviceConnectionConfig<*>
+    ): Result<Unit> {
+        return runCatching {
+            deviceApi.getCompleted()
+        }.transform {
+            it.tryUpdateConnectionConfig(config)
+        }
     }
 
     suspend fun disconnect() {
-        info { "Cancel connect job" }
-        connectJob.cancelAndJoin()
-        if (deviceApi != null) {
-            info { "Find active device api, start disconnect" }
-            deviceApi?.disconnect()
-        }
+        info { "Find active device api, start disconnect" }
+        deviceApi.cancelAndJoin()
+        runCatching {
+            deviceApi.getCompleted()
+        }.getOrNull()?.disconnect()
         info { "Cancel scope" }
         scope.coroutineContext.job.cancelAndJoin()
         scope.cancel()
--- components/bridge/transport/common/api/src/commonMain/kotlin/net/flipper/bridge/connection/transport/common/api/FConnectedDeviceApi.kt
+++ components/bridge/transport/common/api/src/commonMain/kotlin/net/flipper/bridge/connection/transport/common/api/FConnectedDeviceApi.kt
@@ -3,5 +3,14 @@ package net.flipper.bridge.connection.transport.common.api
 interface FConnectedDeviceApi {
     val deviceName: String
 
+    /**
+     * Try to update connection config without interrupting connection
+     *
+     * If it's not possible, return Result.failure() with description
+     */
+    suspend fun tryUpdateConnectionConfig(
+        config: FDeviceConnectionConfig<*>
+    ): Result<Unit>
+
     suspend fun disconnect()
 }
