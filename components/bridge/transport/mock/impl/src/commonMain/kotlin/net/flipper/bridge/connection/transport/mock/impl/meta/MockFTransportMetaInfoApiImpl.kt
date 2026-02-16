86f6ca297b4db8ebc70bf2b900e02d2ca0fcb575
Nikita Kulikov <n@kulikov.uk>
2026-02-16 17:36:21 +0000

    Add ws event

--- components/bridge/transport/mock/impl/src/commonMain/kotlin/net/flipper/bridge/connection/transport/mock/impl/meta/MockFTransportMetaInfoApiImpl.kt
+++ components/bridge/transport/mock/impl/src/commonMain/kotlin/net/flipper/bridge/connection/transport/mock/impl/meta/MockFTransportMetaInfoApiImpl.kt
@@ -21,6 +21,7 @@ class MockFTransportMetaInfoApiImpl : FTransportMetaInfoApi {
             TransportMetaInfoKey.BATTERY_LEVEL -> byteArrayOf(0.5.toInt().toByte())
             TransportMetaInfoKey.BATTERY_POWER_STATE -> byteArrayOf(0)
             TransportMetaInfoKey.EVENTS_INDICATION -> byteArrayOf(0)
+            TransportMetaInfoKey.WS_EVENT -> null
         }
     }
 }
