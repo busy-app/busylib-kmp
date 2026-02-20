253c8cbcdcc122f42ce615f3bce40a1101ee124d
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2026-02-20 18:16:13 +0200

    Update brightness API to use value field (#112)

--- components/bridge/feature/rpc/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/rpc/impl/exposed/FRpcSettingsApiImpl.kt
+++ components/bridge/feature/rpc/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/rpc/impl/exposed/FRpcSettingsApiImpl.kt
@@ -15,7 +15,6 @@ import net.flipper.bridge.connection.feature.rpc.api.model.BsbBrightness
 import net.flipper.bridge.connection.feature.rpc.api.model.DisplayBrightnessInfo
 import net.flipper.bridge.connection.feature.rpc.api.model.NameInfo
 import net.flipper.bridge.connection.feature.rpc.api.model.SuccessResponse
-import net.flipper.bridge.connection.feature.rpc.impl.model.InternalBrightnessInfo
 import net.flipper.core.busylib.ktx.common.cache.ObjectCache
 import net.flipper.core.busylib.ktx.common.cache.getOrElse
 import net.flipper.core.busylib.ktx.common.runSuspendCatching
@@ -45,24 +44,24 @@ class FRpcSettingsApiImpl(
     override suspend fun getDisplayBrightness(ignoreCache: Boolean): Result<DisplayBrightnessInfo> {
         return runSuspendCatching(dispatcher) {
             objectCache.getOrElse(ignoreCache) {
-                httpClient.get("/api/display/brightness").body<InternalBrightnessInfo>()
+                httpClient.get("/api/display/brightness").body<DisplayBrightnessInfo>()
             }
-        }.map { internalBrightnessInfo -> DisplayBrightnessInfo(internalBrightnessInfo.value) }
+        }
     }
 
-    private fun BsbBrightness.asInternalBrightnessInfo(): InternalBrightnessInfo {
+    private fun BsbBrightness.asDisplayBrightnessInfo(): DisplayBrightnessInfo {
         return when (this) {
-            BsbBrightness.Auto -> InternalBrightnessInfo("auto")
-            is BsbBrightness.Number -> InternalBrightnessInfo("${this.value}")
+            BsbBrightness.Auto -> DisplayBrightnessInfo("auto")
+            is BsbBrightness.Number -> DisplayBrightnessInfo("${this.value}")
         }
     }
 
     override suspend fun setDisplayBrightness(
-        front: BsbBrightness,
+        value: BsbBrightness,
     ): Result<SuccessResponse> {
         return runSuspendCatching(dispatcher) {
             httpClient.post("/api/display/brightness") {
-                setBody(front.asInternalBrightnessInfo())
+                setBody(value.asDisplayBrightnessInfo())
             }.body<SuccessResponse>()
         }
     }
