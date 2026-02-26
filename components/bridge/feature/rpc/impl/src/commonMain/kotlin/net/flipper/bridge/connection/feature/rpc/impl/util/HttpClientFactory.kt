14f16e153988cd02c7239d0cff026c7bd48e4cfd
Roman Makeev <57789105+makeevrserg@users.noreply.github.com>
2026-02-26 13:12:49 +0300

    Fix/api responses (#124)
    ## Changers
    - Added default ContentType json
    - Added ApiResponse for less serialization error logging into console
    - Fixed brightness post request

--- components/bridge/feature/rpc/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/rpc/impl/util/HttpClientFactory.kt
+++ components/bridge/feature/rpc/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/rpc/impl/util/HttpClientFactory.kt
@@ -3,10 +3,13 @@ package net.flipper.bridge.connection.feature.rpc.impl.util
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.http.ContentType
+import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import net.flipper.core.busylib.log.info
@@ -22,6 +25,9 @@ internal fun getHttpClient(httpClientEngine: HttpClientEngine) = HttpClient(http
                 coerceInputValues = true
             }
         )
+    }
+    defaultRequest {
+        contentType(ContentType.Application.Json)
     }
     install(Logging) {
         logger = object : Logger {
