127aae7c0551c129ac01a7fa88821451225d2581
Roman Makeev <makeevrserg@gmail.com>
2026-02-26 19:10:03 +0300

    infinite events flow

--- components/bridge/feature/events/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/events/impl/FEventsFeatureApiImpl.kt
+++ components/bridge/feature/events/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/events/impl/FEventsFeatureApiImpl.kt
@@ -1,63 +1,34 @@
 package net.flipper.bridge.connection.feature.events.impl
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.WhileSubscribed
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.merge
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.shareIn
-import me.tatarka.inject.annotations.Assisted
-import me.tatarka.inject.annotations.Inject
+import kotlinx.coroutines.isActive
 import net.flipper.bridge.connection.feature.events.api.ConsumableUpdateEvent
 import net.flipper.bridge.connection.feature.events.api.FEventsFeatureApi
-import net.flipper.bridge.connection.feature.events.impl.bits.BitIndicationEventsFlow
-import net.flipper.bridge.connection.feature.events.impl.ws.WSEventsFlow
-import net.flipper.bridge.connection.transport.common.api.meta.FTransportMetaInfoApi
-import net.flipper.bridge.connection.transport.common.api.meta.TransportMetaInfoKey
+import net.flipper.bridge.connection.feature.events.api.UpdateEvent
 import net.flipper.core.busylib.log.LogTagProvider
-import net.flipper.core.busylib.log.verbose
 import kotlin.time.Duration.Companion.seconds
 
-@Inject
 class FEventsFeatureApiImpl(
-    @Assisted private val metaInfoApi: FTransportMetaInfoApi,
-    @Assisted private val scope: CoroutineScope
+    scope: CoroutineScope
 ) : FEventsFeatureApi, LogTagProvider {
     override val TAG = "FEventsFeatureApi"
-    private val bitIndicationEventsFlow by lazy { BitIndicationEventsFlow() }
-    private val wsEventsFlow by lazy { WSEventsFlow() }
 
-    private val sharedIndicationFlow = combine(
-        metaInfoApi.get(TransportMetaInfoKey.EVENTS_INDICATION),
-        metaInfoApi.get(TransportMetaInfoKey.WS_EVENT)
-    ) { bitsMaskFlowResult, wsEventsFlowResult ->
-        val flows = mutableListOf<Flow<ConsumableUpdateEvent>>(emptyFlow())
-        bitsMaskFlowResult.getOrNull()?.let { flow ->
-            flows += bitIndicationEventsFlow.getEventFlow(flow)
+    private val sharedIndicationFlow = flow {
+        while (currentCoroutineContext().isActive) {
+            delay(5.seconds)
+            UpdateEvent.entries
+                .map { event -> ConsumableUpdateEvent(event, null) }
+                .onEach { event -> emit(event) }
         }
-        wsEventsFlowResult.getOrNull()?.let { flow ->
-            flows += wsEventsFlow.getEventFlow(flow)
-        }
-        flows.merge()
-    }.flatMapLatest { it }
-        .onEach {
-            verbose { "Receive update event: $it" }
-        }
-        .shareIn(scope, SharingStarted.WhileSubscribed(5.seconds))
+    }.shareIn(scope, SharingStarted.WhileSubscribed(5.seconds), 1)
 
     override fun getUpdatesFlow(): Flow<ConsumableUpdateEvent> = sharedIndicationFlow
-
-    @Inject
-    class InternalFactory(
-        private val factory: (FTransportMetaInfoApi, CoroutineScope) -> FEventsFeatureApiImpl
-    ) {
-        operator fun invoke(
-            metaInfoApi: FTransportMetaInfoApi,
-            scope: CoroutineScope
-        ): FEventsFeatureApiImpl = factory(metaInfoApi, scope)
-    }
 }
--- components/bridge/feature/events/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/events/impl/FEventsFeatureFactoryImpl.kt
+++ components/bridge/feature/events/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/events/impl/FEventsFeatureFactoryImpl.kt
@@ -8,23 +8,17 @@ import net.flipper.bridge.connection.feature.common.api.FDeviceFeature
 import net.flipper.bridge.connection.feature.common.api.FDeviceFeatureApi
 import net.flipper.bridge.connection.feature.common.api.FUnsafeDeviceFeatureApi
 import net.flipper.bridge.connection.transport.common.api.FConnectedDeviceApi
-import net.flipper.bridge.connection.transport.common.api.meta.FTransportMetaInfoApi
 import net.flipper.busylib.core.di.BusyLibGraph
 import software.amazon.lastmile.kotlin.inject.anvil.ContributesTo
 
 @Inject
-class FEventsFeatureFactoryImpl(
-    private val eventsFeatureFactory: FEventsFeatureApiImpl.InternalFactory
-) : FDeviceFeatureApi.Factory {
+class FEventsFeatureFactoryImpl : FDeviceFeatureApi.Factory {
     override suspend fun invoke(
         unsafeFeatureDeviceApi: FUnsafeDeviceFeatureApi,
         scope: CoroutineScope,
         connectedDevice: FConnectedDeviceApi
-    ): FDeviceFeatureApi? {
-        return eventsFeatureFactory(
-            metaInfoApi = connectedDevice as? FTransportMetaInfoApi ?: return null,
-            scope = scope
-        )
+    ): FDeviceFeatureApi {
+        return FEventsFeatureApiImpl(scope)
     }
 }
 
