6a60054ffe7d86c0be0ced550e63a07c4d863af0
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2026-02-26 11:09:32 +0200

    And again use CResult for Apple devs (#123)

--- components/bridge/feature/settings/api/src/commonMain/kotlin/net/flipper/bridge/connection/feature/settings/api/FSettingsFeatureApi.kt
+++ components/bridge/feature/settings/api/src/commonMain/kotlin/net/flipper/bridge/connection/feature/settings/api/FSettingsFeatureApi.kt
@@ -4,6 +4,7 @@ import net.flipper.bridge.connection.feature.common.api.FDeviceFeatureApi
 import net.flipper.bridge.connection.feature.rpc.api.model.AudioVolumeInfo
 import net.flipper.bridge.connection.feature.rpc.api.model.BsbBrightness
 import net.flipper.bridge.connection.feature.rpc.api.model.BsbBrightnessInfo
+import net.flipper.busylib.core.wrapper.CResult
 import net.flipper.busylib.core.wrapper.WrappedFlow
 import net.flipper.busylib.core.wrapper.WrappedStateFlow
 
@@ -12,10 +13,10 @@ interface FSettingsFeatureApi : FDeviceFeatureApi {
     fun getBrightnessInfoFlow(): WrappedFlow<BsbBrightnessInfo>
     suspend fun setBrightness(
         value: BsbBrightness,
-    ): Result<Unit>
+    ): CResult<Unit>
 
-    suspend fun setVolume(volume: Int): Result<Unit>
+    suspend fun setVolume(volume: Int): CResult<Unit>
 
     fun getDeviceName(): WrappedStateFlow<String>
-    suspend fun setDeviceName(name: String): Result<Unit>
+    suspend fun setDeviceName(name: String): CResult<Unit>
 }
