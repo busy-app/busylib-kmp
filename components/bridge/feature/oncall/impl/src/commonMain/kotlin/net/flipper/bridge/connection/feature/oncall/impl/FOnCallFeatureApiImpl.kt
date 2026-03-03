679864f462b554b8b916ba5e18ced009d9664194
Nikita Kulikov <nikita@kulikof.ru>
2026-03-03 12:15:01 +0000

    ON Call v2 (#138)

--- components/bridge/feature/oncall/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/oncall/impl/FOnCallFeatureApiImpl.kt
+++ components/bridge/feature/oncall/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/oncall/impl/FOnCallFeatureApiImpl.kt
@@ -1,11 +1,10 @@
 package net.flipper.bridge.connection.feature.oncall.impl
 
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.NonCancellable
-import kotlinx.coroutines.awaitCancellation
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.job
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
 import kotlinx.coroutines.withContext
 import kotlinx.coroutines.withTimeoutOrNull
 import me.tatarka.inject.annotations.Assisted
@@ -20,14 +19,12 @@ import net.flipper.bridge.connection.feature.rpc.api.exposed.FRpcFeatureApi
 import net.flipper.bridge.connection.feature.rpc.api.model.DrawRequest
 import net.flipper.bridge.connection.transport.common.api.FConnectedDeviceApi
 import net.flipper.busylib.core.di.BusyLibGraph
-import net.flipper.core.busylib.ktx.common.DelicateSingleJobApi
 import net.flipper.core.busylib.ktx.common.SingleJobMode
 import net.flipper.core.busylib.ktx.common.asSingleJobScope
-import net.flipper.core.busylib.ktx.common.exponentialRetry
+import net.flipper.core.busylib.ktx.common.cancelPrevious
 import net.flipper.core.busylib.log.LogTagProvider
 import net.flipper.core.busylib.log.error
 import software.amazon.lastmile.kotlin.inject.anvil.ContributesTo
-import kotlin.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
 @Inject
@@ -39,78 +36,59 @@ class FOnCallFeatureApiImpl(
     override val TAG: String = "FOnCallFeatureApi"
 
     private val singleJobScope = scope.asSingleJobScope()
-    private var startJob: Job? = null // todo replace with singleJobScope job
 
-    @OptIn(DelicateSingleJobApi::class)
-    override suspend fun start() {
-        startJob = runCatching {
-            singleJobScope.withJobMode(SingleJobMode.SKIP_IF_RUNNING) {
-                try {
-                    exponentialRetry(
-                        initialDelay = INITIAL_RETRY_DELAY,
-                        maxDelay = MAX_RETRY_DELAY
-                    ) {
-                        performStartAttempt()
-                    }
-                    awaitCancellation()
-                } finally {
-                    withContext(NonCancellable) {
-                        withTimeoutOrNull(3.seconds) {
-                            performStopAttempt()
-                        }
+    override fun start() {
+        singleJobScope.withJobMode(SingleJobMode.CANCEL_PREVIOUS) {
+            try {
+                while (currentCoroutineContext().isActive) {
+                    rpcFeatureApi
+                        .fRpcAssetsApi
+                        .displayDraw(createDrawRequest())
+                        .onFailure { error(it) { "Failed to display draw" } }
+                    delay(UPDATE_DELAY)
+                }
+            } finally {
+                withContext(NonCancellable) {
+                    withTimeoutOrNull(3.seconds) {
+                        performStopAttempt()
                     }
                 }
-            }.job
-        }.getOrNull()
+            }
+        }
     }
 
-    override suspend fun stop() {
-        val job = startJob
-        job?.cancelAndJoin()
-    }
-
-    private suspend fun performStartAttempt(): Result<Unit> = runCatching {
-        rpcFeatureApi
-            .fRpcAssetsApi
-            .uploadAsset(
-                appId = OnCallImage.APP_ID,
-                file = OnCallImage.IMAGE_NAME,
-                content = OnCallImage.content
-            )
-            .onFailure { error(it) { "Failed to upload asset" } }
-            .getOrThrow()
-
-        rpcFeatureApi
-            .fRpcAssetsApi
-            .displayDraw(createDrawRequest())
-            .onFailure { error(it) { "Failed to display draw" } }
-            .getOrThrow()
+    override fun stop() {
+        singleJobScope.cancelPrevious()
     }
 
     private suspend fun performStopAttempt(): Result<Unit> {
-        return rpcFeatureApi.fRpcAssetsApi.removeDraw(appId = OnCallImage.APP_ID).map { }
+        return rpcFeatureApi.fRpcAssetsApi.removeDraw(appId = APP_ID).map { }
     }
 
     private fun createDrawRequest(): DrawRequest {
         return DrawRequest(
-            appId = OnCallImage.APP_ID,
+            appId = APP_ID,
             elements = listOf(
                 DrawRequest.Element(
                     id = "0",
-                    timeout = OnCallImage.TIMEOUT,
-                    type = DrawRequest.Element.ElementType.IMAGE,
-                    x = 0,
-                    y = 0,
-                    path = OnCallImage.IMAGE_NAME,
-                    display = DrawRequest.Display.FRONT
+                    timeout = ELEMENT_TIMEOUT,
+                    priority = ELEMENT_PRIORITY,
+                    display = DrawRequest.Display.FRONT,
+                    type = DrawRequest.Element.ElementType.ANIM,
+                    builtinAnim = BUILTIN_ANIM,
+                    section = "loop",
+                    loop = true
                 )
             )
         )
     }
 
     companion object {
-        private val INITIAL_RETRY_DELAY = 1.seconds
-        private val MAX_RETRY_DELAY = Duration.INFINITE
+        private const val APP_ID = "on_call"
+        private const val BUILTIN_ANIM = "shared/on_call_72x16"
+        private const val ELEMENT_TIMEOUT = 30
+        private const val ELEMENT_PRIORITY = 50
+        private val UPDATE_DELAY = 3.seconds
     }
 
     @Inject
