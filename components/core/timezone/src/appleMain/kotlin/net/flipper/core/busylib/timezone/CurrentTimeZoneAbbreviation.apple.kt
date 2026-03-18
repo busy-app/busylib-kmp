053c2bc643e47e033c301036da22ec12b55cd8a2
Nikita Kulikov <n@kulikov.uk>
2026-03-18 18:14:55 +0000

    Fix apple

--- components/core/timezone/src/appleMain/kotlin/net/flipper/core/busylib/timezone/CurrentTimeZoneAbbreviation.apple.kt
+++ components/core/timezone/src/appleMain/kotlin/net/flipper/core/busylib/timezone/CurrentTimeZoneAbbreviation.apple.kt
@@ -1,6 +1,8 @@
 package net.flipper.core.busylib.timezone
 
 import platform.Foundation.NSTimeZone
+import platform.Foundation.abbreviation
+import platform.Foundation.localTimeZone
 
 actual fun currentTimeZoneAbbreviation(): String {
     return NSTimeZone.localTimeZone.abbreviation ?: ""
