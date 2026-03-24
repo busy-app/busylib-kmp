6bd0159234d3b2e23927456b762a7fab61830cf1
Nikita Kulikov <n@kulikov.uk>
2026-03-24 13:03:04 +0000

    Fix javadoc

--- components/bridge/feature/rpc/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/rpc/impl/util/throttle/HttpRequestThrottle.kt
+++ components/bridge/feature/rpc/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/rpc/impl/util/throttle/HttpRequestThrottle.kt
@@ -13,11 +13,6 @@ import io.ktor.http.HttpStatusCode.Companion.TooManyRequests
  * Kind of the counterpart to the
  * [Ktor Rate Limiting Server Plugin](https://ktor.io/docs/server-rate-limit.html).
  *
- * The default [HttpRequestThrottler] does not throttle and only takes care of automatic
- * retries. If the server responds with `429` [TooManyRequests] and the `Retry-After` header,
- * then the original request is automatically retried according to the delay defined in the
- * header.
- *
  * Throttling can be configured via the [HttpRequestThrottleConfig.throttler] config property.
  *
  * You can also implement fully custom throttling logic by building a custom [HttpRequestThrottler]
