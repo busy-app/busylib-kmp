00be6c22cf30ae3a79d21d21bb26afc0b16f3e99
Programistich <35292229+Programistich@users.noreply.github.com>
2026-03-24 15:14:49 +0200

    Function for get observable settings

--- entrypoint/src/appleMain/kotlin/net/flipper/busylib/IOSAppComponent.kt
+++ entrypoint/src/appleMain/kotlin/net/flipper/busylib/IOSAppComponent.kt
@@ -1,21 +1,15 @@
 package net.flipper.busylib
 
 import com.russhwolf.settings.NSUserDefaultsSettings
+import com.russhwolf.settings.ObservableSettings
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
-import net.flipper.bridge.connection.config.api.FDevicePersistedStorage
-import net.flipper.bridge.connection.config.impl.FDevicePersistedStorageImpl
-import net.flipper.bsb.auth.principal.api.BUSYLibPrincipalApi
 import platform.Foundation.NSUserDefaults
 
 fun getBUSYLibIOSScope(): CoroutineScope {
     return CoroutineScope(SupervisorJob())
 }
 
-fun getPersistedStorage(delegate: NSUserDefaults): FDevicePersistedStorage {
-    return FDevicePersistedStorageImpl(NSUserDefaultsSettings(delegate))
-}
-
-fun getBsbUserPrincipalApi(): BUSYLibPrincipalApi {
-    return BUSYLibPrincipalApiImpl()
+fun getObservationSettings(delegate: NSUserDefaults): ObservableSettings {
+    return NSUserDefaultsSettings(delegate = delegate)
 }
