86d8045adc2871f084604d4628643130ac5ea2d7
Nikita Kulikov <n@kulikov.uk>
2026-03-26 11:16:46 +0000

    Add new FEventsFeatureApi extension functions

--- components/bridge/feature/events/api/src/commonMain/kotlin/net/flipper/bridge/connection/feature/events/model/BsbUpdateEvent.kt
+++ components/bridge/feature/events/api/src/commonMain/kotlin/net/flipper/bridge/connection/feature/events/model/BsbUpdateEvent.kt
@@ -3,6 +3,7 @@ package net.flipper.bridge.connection.feature.events.model
 /**
  * Update events which goes only from BusyBar (socket/lan/cloud)
  */
+@Deprecated("Use BusyLibUpdateEvent instead")
 enum class BsbUpdateEvent(
     val bitIndex: Int? = null,
     val webSocketKey: String? = null
