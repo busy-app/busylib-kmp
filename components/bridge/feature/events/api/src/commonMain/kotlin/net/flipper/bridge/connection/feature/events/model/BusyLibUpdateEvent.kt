5be825dc3c01a76453b98e9187dc646cd45eb643
Nikita Kulikov <n@kulikov.uk>
2026-04-07 09:12:45 +0100

    Fix BusyLibUpdateEvent.Wifi

--- components/bridge/feature/events/api/src/commonMain/kotlin/net/flipper/bridge/connection/feature/events/model/BusyLibUpdateEvent.kt
+++ components/bridge/feature/events/api/src/commonMain/kotlin/net/flipper/bridge/connection/feature/events/model/BusyLibUpdateEvent.kt
@@ -19,9 +19,21 @@ sealed interface BusyLibUpdateEvent : UpdateEvent {
     ) : BusyLibUpdateEvent
 
     data class Wifi(
-        val isConnected: Boolean,
+        val state: State,
         val ssid: String?,
-    ) : BusyLibUpdateEvent
+        val bssid: String?,
+        val channel: Int?,
+        val rssi: Int?,
+    ) : BusyLibUpdateEvent {
+        enum class State {
+            UNKNOWN,
+            DISCONNECTED,
+            CONNECTED,
+            CONNECTING,
+            DISCONNECTING,
+            RECONNECTING,
+        }
+    }
 
     data class UpdateState(
         val action: Action,
