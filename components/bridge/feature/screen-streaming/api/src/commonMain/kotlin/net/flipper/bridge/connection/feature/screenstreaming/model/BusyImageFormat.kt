3abe626023e487c8d9ce263ac9d8ee5efdcd4e78
Nikita Kulikov <n@kulikov.uk>
2026-04-09 09:05:32 +0100

    Fix BusyImageFormat

--- components/bridge/feature/screen-streaming/api/src/commonMain/kotlin/net/flipper/bridge/connection/feature/screenstreaming/model/BusyImageFormat.kt
+++ components/bridge/feature/screen-streaming/api/src/commonMain/kotlin/net/flipper/bridge/connection/feature/screenstreaming/model/BusyImageFormat.kt
@@ -8,7 +8,7 @@ package net.flipper.bridge.connection.feature.screenstreaming.model
 data class BusyImageFormat(val byteArray: ByteArray) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+        if (other == null || this::class != other::class) return false
 
         other as BusyImageFormat
 
