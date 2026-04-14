c421b1d0df93de3103e077a6b89409cd0d2496f6
Roman Makeev <57789105+makeevrserg@users.noreply.github.com>
2026-04-14 15:42:37 +0300

    fix PreviousVersionFlowProvider.kt (#236)
    ## Summary
    This PR fixes wrong version transition when BusyBar is switched

--- components/bridge/device/firmware-update/impl/src/commonMain/kotlin/net/flipper/bridge/device/firmwareupdate/updater/api/PreviousVersionFlowProvider.kt
+++ components/bridge/device/firmware-update/impl/src/commonMain/kotlin/net/flipper/bridge/device/firmwareupdate/updater/api/PreviousVersionFlowProvider.kt
@@ -9,11 +9,11 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.distinctUntilChangedBy
 import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onEach
 import me.tatarka.inject.annotations.Inject
+import net.flipper.bridge.connection.config.api.FDevicePersistedStorage
 import net.flipper.bridge.connection.feature.info.api.FDeviceInfoFeatureApi
 import net.flipper.bridge.connection.feature.provider.api.FFeatureProvider
 import net.flipper.bridge.connection.feature.provider.api.FFeatureStatus
@@ -26,15 +26,25 @@ import net.flipper.core.busylib.ktx.common.tryCast
 
 @Inject
 class PreviousVersionFlowProvider(
-    private val fFeatureProvider: FFeatureProvider
+    private val fFeatureProvider: FFeatureProvider,
+    private val fDevicePersistedStorage: FDevicePersistedStorage
 ) {
-    private fun getPreviousVersionFlow() = flow(
+    private fun getPreviousVersionFlow() = channelFlow(
         block = {
-            emit(null)
+            send(null)
             var busyBarVersionTransitionOrNull: BusyBarVersionTransition? = null
-            fFeatureProvider.get<FDeviceInfoFeatureApi>()
-                .map { status -> status.tryCast<FFeatureStatus.Supported<FDeviceInfoFeatureApi>>() }
-                .flatMapLatest { status -> status?.featureApi?.deviceVersionFlow.orNullable() }
+            // Reset versionTransition on BusyBar change
+            fDevicePersistedStorage.getCurrentDeviceFlow()
+                .distinctUntilChanged()
+                .onEach {
+                    send(null)
+                    busyBarVersionTransitionOrNull = null
+                }
+                .flatMapLatest {
+                    fFeatureProvider.get<FDeviceInfoFeatureApi>()
+                        .map { status -> status.tryCast<FFeatureStatus.Supported<FDeviceInfoFeatureApi>>() }
+                        .flatMapLatest { status -> status?.featureApi?.deviceVersionFlow.orNullable() }
+                }
                 .distinctUntilChanged()
                 .onEach { newCurrentVersion ->
                     val versionModel = VersionsModelDiff.compareAndGet(
@@ -42,7 +52,7 @@ class PreviousVersionFlowProvider(
                         newCurrentVersion = newCurrentVersion
                     )
                     busyBarVersionTransitionOrNull = versionModel
-                    emit(versionModel)
+                    send(versionModel)
                 }
                 .collect()
         }
