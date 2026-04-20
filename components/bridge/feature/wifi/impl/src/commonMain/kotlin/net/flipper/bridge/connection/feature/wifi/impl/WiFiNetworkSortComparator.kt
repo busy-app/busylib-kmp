a25215a1304699eb2f377aabc109f426774a9e97
Dzhos Oleksii <35292229+Programistich@users.noreply.github.com>
2026-04-20 15:42:13 +0300

    Sort Wi-Fi networks by security type, then by RSSI (#248)

--- components/bridge/feature/wifi/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/wifi/impl/WiFiNetworkSortComparator.kt
+++ components/bridge/feature/wifi/impl/src/commonMain/kotlin/net/flipper/bridge/connection/feature/wifi/impl/WiFiNetworkSortComparator.kt
@@ -1,8 +1,16 @@
 package net.flipper.bridge.connection.feature.wifi.impl
 
 import net.flipper.bridge.connection.feature.wifi.api.model.WiFiNetwork
+import net.flipper.bridge.connection.feature.wifi.api.model.WiFiSecurity
 
 @Suppress("FunctionNaming")
 internal fun WiFiNetworkSortComparator(): Comparator<WiFiNetwork> {
-    return Comparator { first, second -> second.rssi.compareTo(first.rssi) }
+    return compareBy<WiFiNetwork> { it.wifiSecurity.sortPriority() }
+        .thenByDescending { it.rssi }
+        .thenBy { it.ssid }
+}
+
+private fun WiFiSecurity.sortPriority(): Int = when (this) {
+    is WiFiSecurity.Supported -> 0
+    is WiFiSecurity.Other -> 1
 }
