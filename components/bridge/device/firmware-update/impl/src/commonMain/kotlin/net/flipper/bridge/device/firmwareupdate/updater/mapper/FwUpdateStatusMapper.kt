8d934f6e947e66e1d023400e14e823865361c1c6
Nikita Kulikov <n@kulikov.uk>
2026-05-07 10:45:44 +0100

    Start impl for FwUpdateStatusMapper

--- components/bridge/device/firmware-update/impl/src/commonMain/kotlin/net/flipper/bridge/device/firmwareupdate/updater/mapper/FwUpdateStatusMapper.kt
+++ components/bridge/device/firmware-update/impl/src/commonMain/kotlin/net/flipper/bridge/device/firmwareupdate/updater/mapper/FwUpdateStatusMapper.kt
@@ -8,19 +8,47 @@ import net.flipper.bridge.device.firmwareupdate.updater.model.FwUpdateState
 import net.flipper.bridge.device.firmwareupdate.uploader.model.FirmwareUploaderState
 
 internal object FwUpdateStatusMapper {
-    private fun fromCheckStatus(
+    fun from(
         updateStatus: BsbUpdateStatus,
-        availableVersion: String?
+        availableVersion: BsbUpdateVersion,
+        uploaderState: FirmwareUploaderState,
+        downloaderState: FirmwareDownloaderState,
     ): FwUpdateState {
-        return when(updateStatus) {
+        when(uploaderState) {
+            FirmwareUploaderState.Failed,
+            FirmwareUploaderState.Pending -> TODO()
+            FirmwareUploaderState.Uploaded -> TODO()
+            is FirmwareUploaderState.Uploading -> TODO()
+        }
+        return when (availableVersion) {
+            BsbUpdateVersion.CheckingOnBBInProgress -> FwUpdateState.CheckingVersion
+            BsbUpdateVersion.FailedToCheck -> FwUpdateState.CouldNotCheckUpdate
+            BsbUpdateVersion.Loading -> FwUpdateState.Pending
+            BsbUpdateVersion.NoUpdateAvailable -> FwUpdateState.NoUpdateAvailable
+            is BsbUpdateVersion.ReadyToUpdate.Default -> TODO()
+            is BsbUpdateVersion.ReadyToUpdate.Url -> TODO()
+        }
+    }
+
+    private fun from(
+        updateStatus: BsbUpdateStatus,
+        uploaderState: FirmwareUploaderState,
+        downloaderState: FirmwareDownloaderState
+    ) {
+        when (uploaderState) {
+            FirmwareUploaderState.Failed -> TODO()
+            FirmwareUploaderState.Pending -> TODO()
+            FirmwareUploaderState.Uploaded -> TODO()
+            is FirmwareUploaderState.Uploading -> TODO()
+        }
+        when (updateStatus) {
             is BsbUpdateStatus.FailedUpdate -> FwUpdateState.Failure
             BsbUpdateStatus.InProgress.Downloading.NotSpecified -> TODO()
             is BsbUpdateStatus.InProgress.Downloading.Specified -> TODO()
-            is BsbUpdateStatus.InProgress.Other -> TODO()
+            is BsbUpdateStatus.InProgress.Other -> FwUpdateState.Updating
             BsbUpdateStatus.ReadyToInstall.BatteryLow -> FwUpdateState.LowBattery
             BsbUpdateStatus.ReadyToInstall.Ready -> TODO()
-            BsbUpdateStatus.InProgress.CheckingInProgress -> FwUpdateState.CheckingVersion
-            BsbUpdateStatus.Loading -> TODO()
+            BsbUpdateStatus.Loading -> FwUpdateState.Pending
         }
     }
 }
