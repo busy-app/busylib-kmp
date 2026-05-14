332f6034e39095a1a39b6ec5aa1d40767d9c53db
Nikita Kulikov <n@kulikov.uk>
2026-05-14 12:43:46 +0100

    Limit maximum connections per host for Darwin engine factory

--- components/core/ktor/src/appleMain/kotlin/net/flipper/core/ktor/HttpEnginePlatform.apple.kt
+++ components/core/ktor/src/appleMain/kotlin/net/flipper/core/ktor/HttpEnginePlatform.apple.kt
@@ -1,8 +1,26 @@
 package net.flipper.core.ktor
 
+import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.HttpClientEngineFactory
 import io.ktor.client.engine.darwin.Darwin
+import io.ktor.client.engine.darwin.DarwinClientEngineConfig
 
-actual fun getPlatformEngineFactory(): HttpClientEngineFactory<*> {
-    return Darwin
+/**
+ * Cap NSURLSession's HTTP/1.1 keep-alive pool per host. Combined with the
+ * dedicated [FAppleLanConnectionMonitor] probe and the WebSocket task, this
+ * bounds total TCP sockets per BUSY Bar to roughly 4.
+ */
+private const val MAX_CONNECTIONS_PER_HOST: Long = 3
+
+actual fun getPlatformEngineFactory(): HttpClientEngineFactory<*> = ConfiguredDarwinFactory
+
+private object ConfiguredDarwinFactory : HttpClientEngineFactory<DarwinClientEngineConfig> {
+    override fun create(block: DarwinClientEngineConfig.() -> Unit): HttpClientEngine {
+        return Darwin.create {
+            block()
+            configureSession {
+                HTTPMaximumConnectionsPerHost = MAX_CONNECTIONS_PER_HOST
+            }
+        }
+    }
 }
