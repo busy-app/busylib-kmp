65d3368b86ca00220c32a7e99c3448a01f1703d7
Nikita Kulikov <n@kulikov.uk>
2026-06-25 11:52:38 +0100

    Add suppress to BsbFirmwareUpdateFileType

--- components/cloud/rest/api/src/commonMain/kotlin/net/flipper/bsb/cloud/rest/model/BsbFirmwareUpdateFileType.kt
+++ components/cloud/rest/api/src/commonMain/kotlin/net/flipper/bsb/cloud/rest/model/BsbFirmwareUpdateFileType.kt
@@ -1,15 +1,11 @@
 package net.flipper.bsb.cloud.rest.model
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import net.flipper.bsb.cloud.rest.serialization.FileTypeSerializer
 
 @Serializable(with = FileTypeSerializer::class)
+@Suppress("SerialNameNotProvidedRule")
 enum class BsbFirmwareUpdateFileType {
-
-    @SerialName("update_signed_tgz")
     UPDATE_SIGNED_TGZ,
-
-    @SerialName("UNKNOWN")
     UNKNOWN
 }
