296dc5b965047fe82514ee4265caf6b2d3ca6003
Roman Makeev <makeevrserg@gmail.com>
2026-07-07 15:13:28 +0300

    [MOB-2790] Fix gradle dependencies

--- components/tools/oncall/impl/build.gradle.kts
+++ components/tools/oncall/impl/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("flipper.multiplatform")
     id("flipper.metro-multiplatform")
+    id("kotlinx-serialization")
 }
 
 kotlin {
@@ -9,12 +10,26 @@ kotlin {
 
         implementation(projects.components.core.di)
         implementation(projects.components.core.ktx)
+        implementation(projects.components.core.ktor)
         implementation(projects.components.core.log)
+        implementation(projects.components.core.wrapper)
 
+        implementation(projects.components.bridge.config.api)
+        implementation(projects.components.bridge.lanmonitor.api)
+        implementation(projects.components.bridge.orchestrator.api)
+        implementation(projects.components.bridge.transport.tcp.cloud.impl)
+        implementation(projects.components.bridge.transport.tcp.lan.impl)
         implementation(projects.components.bridge.feature.common.api)
         implementation(projects.components.bridge.feature.provider.api)
         implementation(projects.components.bridge.feature.oncall.api)
+        implementation(projects.components.bridge.feature.oncall.impl)
+        implementation(projects.components.bridge.feature.rpc.api)
+        implementation(projects.components.bridge.feature.rpc.impl)
 
         implementation(libs.kotlin.coroutines)
+        implementation(libs.kotlin.serialization.json)
+        implementation(libs.ktor.client.core)
+        implementation(libs.ktor.negotiation)
+        implementation(libs.ktor.serialization)
     }
 }
